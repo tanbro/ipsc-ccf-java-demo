1105b50db187124c01e96b151d095f7e05b5c99a
tanbr <RealTanbro@Gmail.com>
2017-05-10 15:56:45 +0800

    ...

--- ipsc-ccf-demo-conference/src/main/java/com/hesong/ipsc/ccf/demo/conference/Main.java
+++ ipsc-ccf-demo-conference/src/main/java/com/hesong/ipsc/ccf/demo/conference/Main.java
@@ -20,48 +20,14 @@ public class Main {
     private static final byte localId = 24;
     private static final byte commanderId = 10;
 
-    private static boolean isFirstConnected = false;
     private static Commander commander;
+    private static String conferenceId;
 
     public static void main(String[] args) throws InterruptedException {
         logger.info("Data Bus 客户端单元初始化");
         Unit.initiate(localId, new UnitCallbacks() {
             public void connectSucceed(Client client) {
                 logger.info("成功的连接到了IPSC服务程序的 Data Bus");
-                if (client.getId() == commander.getId()) {
-                    if (!isFirstConnected) {
-                        isFirstConnected = true;
-                        logger.info("第一次连接，新建一个会议资源");
-                        try {
-                            Map<String, Object> params = new HashMap<String, Object>();
-                            params.put("max_seconds", 300); /// 会议最长时间，这是必填参数
-                            commander.createResource(
-                                    new BusAddress(client.getConnectingUnitId(), (byte) 0),
-                                    "sys.conf",
-                                    params,
-                                    new RpcResultListener() {
-                                        @Override
-                                        protected void onResult(Object o) {
-
-                                        }
-
-                                        @Override
-                                        protected void onError(RpcError rpcError) {
-
-                                        }
-
-                                        @Override
-                                        protected void onTimeout() {
-
-                                        }
-                                    }
-                            );
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-
-                }
             }
 
             public void connectFailed(Client client, int i) {
@@ -87,11 +53,41 @@ public class Main {
         /// 开始执行
         String inputStr;
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("会议DEMO开始了! 输入 \"quit\" 或者 \"q\" 退出程序.\n");
+        System.out.printf("会议DEMO开始了! 输入 \"c\" 建立会议.\n");
         while (true) {
             inputStr = scanner.nextLine();
-            if (inputStr.trim().toLowerCase().startsWith("q")) {
-                break;
+            if (inputStr.trim().toLowerCase().startsWith("c")) {
+                logger.info("建立会议");
+                try {
+                    Map<String, Object> params = new HashMap<String, Object>();
+                    params.put("max_seconds", 300); /// 会议最长时间，这是必填参数
+                    commander.createResource(
+                            new BusAddress(commander.getConnectingUnitId(), (byte) 0),
+                            "sys.conf",
+                            params,
+                            new RpcResultListener() {
+                                @Override
+                                protected void onResult(Object o) {
+                                    Map<String, Object> result = (Map<String, Object>) o;
+                                    conferenceId = (String) result.get("res_id");
+                                    logger.info("会议建立成功，ID=%s", conferenceId);
+                                }
+
+                                @Override
+                                protected void onError(RpcError rpcError) {
+                                    logger.error("创建会议资源错误：%d %s", rpcError.getCode(), rpcError.getMessage());
+                                }
+
+                                @Override
+                                protected void onTimeout() {
+                                    logger.error("创建会议资源超时无响应");
+                                }
+                            }
+                    );
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
         }
 
